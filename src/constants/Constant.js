460f2cef7933d5ef96313eefabc8b5531731d22c
ajshan23 <ajmalshahan23@gmail.com>
2024-12-25 17:05:10 +0530

    edited routes for proper handling

--- src/constants/Constant.js
+++ src/constants/Constant.js
@@ -1,2 +1,2 @@
-// export const apiBaseUrl = "http://ec2-13-232-208-24.ap-south-1.compute.amazonaws.com/api"
-export const apiBaseUrl = "http://localhost:5000/api"
+export const apiBaseUrl = "http://ec2-43-204-2-117.ap-south-1.compute.amazonaws.com:5000/api"
+// export const apiBaseUrl = "http://localhost:5000/api"
